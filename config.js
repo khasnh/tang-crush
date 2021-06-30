c3c742843ef25331c4bb47000a99b74f9e58ae18
khasnh <86723258+khasnh@users.noreply.github.com>
2021-06-30 19:38:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,12 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'oh bấy bì!',
+    introDesc: `zời ơi e rảnh 
+    qua' nên đi test :v `,
+    btnIntro: 'bấm vào đay nek',
+    title: 'anh zai có iu iem hơm',
+    desc: 'chọn e hoặc chj Chi =)))) ',
+    btnYes: 'iêu iem',
+    btnNo: 'iêu chj Chi',
+    question:'shao chọn iem dọ ><',
+    btnReply: 'bấm vào đây dể gửi',
+    reply: 'vì chỉ có đàn ông mới đem lại hp cho nhau <3'
